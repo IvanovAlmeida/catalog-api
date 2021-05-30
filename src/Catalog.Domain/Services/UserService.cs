7d63f283e314cce343500e577a5dbd8d63ba398e
Ivanov de Almeida <ivanovdealmeida@gmail.com>
2021-05-30 18:41:53 -0300

    Add ItemValidationTest

--- src/Catalog.Domain/Services/UserService.cs
+++ src/Catalog.Domain/Services/UserService.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Linq;
 using System.Threading.Tasks;
 using Catalog.Domain.Interfaces;
 using Catalog.Domain.Models;
---
+++ tests/Catalog.Domain.Tests/Validations/ItemValidationTest.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+using System.Linq;
+
+using Catalog.Domain.Models;
+using Catalog.Domain.Models.Validations;
+
+namespace Catalog.Domain.Tests.Validations
+{
+    public class ItemValidationTest
+    {
+        [Fact(DisplayName = "Item valid must return true")]
+        [Trait("Categoria", "Validation - Item")]
+        public void ItemValidation_ItemValid_MustReturnTrue()
+        {
+            // Arrange
+            var itemValidation = new ItemValidation();
+            var item = new Item
+            {
+                Name = "Item 1",
+                ItemType = ItemType.Product,
+                Value = 25.00m
+            };
+
+            // Act
+            var result = itemValidation.Validate(item);
+
+            // Assert
+            Assert.True(result.IsValid);
+            Assert.Empty(result.Errors);
+        }
+
+        [Fact(DisplayName = "Item invalid must return false")]
+        [Trait("Categoria", "Validation - Item")]
+        public void ItemValidation_ItemInvalid_MustReturnFalse()
+        {
+            // Arrange
+            var itemValidation = new ItemValidation();
+            var item = new Item
+            {
+                Name = "",
+                Value = 0
+            };
+
+            // Act
+            var result = itemValidation.Validate(item);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal(3, result.Errors.Count);
+            Assert.Contains(ItemValidation.ErroMsgEmptyName, result.Errors.Select(m => m.ErrorMessage));
+            Assert.Contains(ItemValidation.ErroMsgItemTypeInvalid, result.Errors.Select(m => m.ErrorMessage));
+            Assert.Contains(ItemValidation.ErroMsgValueGreaterThan, result.Errors.Select(m => m.ErrorMessage));
+        }
+    }
+}
